58e0059789b75e5638f36b94c64a098c80200d74
Dobromir Kovachev <dobromir.mail@gmail.com>
2023-12-30 02:40:39 +0200

    Simple state sharing betwen components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,13 @@ import Message from "./components/ListGroup";
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button/Button";
+import Like from "./components/Like";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 function App() {
     const [alertVisible, setAlertVisibility] = useState(false);
+    const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
     let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
     const handleSelectItem = (item: string) => {
@@ -16,6 +20,9 @@ function App() {
 
     return (
         <div>
+            <NavBar cartItemsCount={cartItems.length} />
+            <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+            <Like />
             <Button color="primary" onClick={() => setAlertVisibility(true)}>
                 My Button
             </Button>
---
+++ frontend/src/components/Like.tsx
@@ -0,0 +1,7 @@
+import { TfiYoutube } from "react-icons/tfi";
+
+const Like = () => {
+    return <TfiYoutube color="brown" size="200" />;
+};
+
+export default Like;
