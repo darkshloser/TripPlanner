2bd04df255f226a9dd7417975d347a9e9860f4b0
Dobromir Kovachev <dobromir.mail@gmail.com>
2024-01-04 03:52:48 +0200

    Create a new widget 'expense-tracker'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,10 @@ import Cart from "./components/Cart";
 import Form from "./components/Form";
 import FormState from "./components/FormState";
 import FormReactHook from "./components/FormReactHook";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/categories";
 
 function App() {
     const [alertVisible, setAlertVisibility] = useState(false);
@@ -21,8 +25,47 @@ function App() {
         console.log(item);
     };
 
+    const [selectedCategory, setSelectedCategory] = useState("");
+
+    const [expenses, setExpenses] = useState([
+        { id: 1, description: "desc text", amount: 10, category: "Utilities" },
+        {
+            id: 2,
+            description: "desc 2 text",
+            amount: 102,
+            category: "Utilities",
+        },
+    ]);
+
+    const visibleExpenses = selectedCategory
+        ? expenses.filter((e) => e.category === selectedCategory)
+        : expenses;
+
     return (
         <div>
+            <div className="mb-5">
+                <ExpenseForm
+                    onSubmit={(expense) =>
+                        setExpenses([
+                            ...expenses,
+                            { ...expense, id: expenses.length + 1 },
+                        ])
+                    }
+                />
+            </div>
+            <div className="mb-3">
+                <ExpenseFilter
+                    onSelectCategory={(category) =>
+                        setSelectedCategory(category)
+                    }
+                />
+            </div>
+            <ExpenseList
+                expenses={visibleExpenses}
+                onDelete={(id) =>
+                    setExpenses(expenses.filter((e) => e.id !== id))
+                }
+            />
             <NavBar cartItemsCount={cartItems.length} />
             <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
             <Form />
